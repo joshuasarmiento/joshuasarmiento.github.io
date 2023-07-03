c5fff1610414f9cd267d6b2222d4c41f46f8cd53
Joshua Sarmiento <sarmientojoshua@gmail.com>
2023-07-03 22:26:27 +0800

    prog lang

---
+++ src/views/components/About/ProgLanguages.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="animate-fd-4 mt-4 animate-fd-9 rounded-md md:rounded-lg max-w-none">
+        <div class="overflow-x-auto">
+            <div v-if="isDark" class="whitespace-nowrap">
+                <figure class="">
+                    <embed loading="lazy" :src="imageUrlD"/>              
+                </figure>
+            </div>
+            <div v-else class="whitespace-nowrap">
+                <figure class="">
+                    <embed loading="lazy" :src="imageUrlL" />              
+                </figure>
+            </div>
+        </div> 
+    </section>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import {
+    useDark,
+} from "@vueuse/core";
+
+const isDark = useDark();
+const imageUrlD = ref("https://wakatime.com/share/@returnjosh/33ac2b63-f26e-46d0-b8cc-d4484c5f2691.svg");
+const imageUrlL = ref("https://wakatime.com/share/@returnjosh/bfee05c3-a18c-4a10-950a-818af4ec0a93.svg")
+</script>
+
+<style scoped>
+
+</style>
