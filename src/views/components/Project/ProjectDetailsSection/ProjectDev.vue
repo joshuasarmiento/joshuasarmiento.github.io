6e7a7ca478b9661e76644b13d6f56f451b37e7e6
Joshua Sarmiento <supremojoshua22@gmail.com>
2023-07-04 15:01:11 +0800

    add badge to project detail

--- src/views/components/Project/ProjectDetailsSection/ProjectDev.vue
+++ src/views/components/Project/ProjectDetailsSection/ProjectDev.vue
@@ -3,7 +3,15 @@
         <div class="flex flex-col gap-20">
             <article>
                 <div class="flex flex-col gap-3">
-                    <p class="text-secondary animate-fade-up"><time datetime="2023-03-05">{{ data.date }}</time></p>
+                    <div class="flex space-x-4">
+                        <p class="text-secondary animate-fade-up"><time datetime="2023-03-05">{{ data.date }}</time></p>
+                        <span v-if="data.isOngoing" class="animate-fade-up inline-flex items-center justify-center rounded-full bg-amber-100 px-2.5 py-0.5 text-amber-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="-ms-1 me-1.5 h-4 w-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9.75h4.875a2.625 2.625 0 010 5.25H12M8.25 9.75L10.5 7.5M8.25 9.75L10.5 12m9-7.243V21.75l-3.75-1.5-3.75 1.5-3.75-1.5-3.75 1.5V4.757c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0c1.1.128 1.907 1.077 1.907 2.185z" />
+                            </svg>
+                            <p class="whitespace-nowrap text-sm">Ongoing</p>
+                        </span>
+                    </div>
                     <h1 class="text-primary animate-fd-1 text-3xl font-bold tracking-tight leading-tight">{{ data.subject }}</h1>
                     <p class="text-xl text-secondary animate-fd-2 ">{{ data.titleDesc }}</p>
                 </div>
